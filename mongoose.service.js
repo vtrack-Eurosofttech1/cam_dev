0eed2522f3aedb99446473d32648c55fa6135c21
Eurosoft <148074912+vtrack-Eurosofttech1@users.noreply.github.com>
2025-07-01 18:45:28 +0500

    update mongoose.service

--- mongoose.service.js
+++ mongoose.service.js
@@ -16,11 +16,10 @@ const connectWithRetry = async() => {
     console.log("🟢 Not Already connected to MongoDB.");
 
   }
-
   const uri =
   process.env.ENVIRONMENT === "DEVELOPMENT" ? 
-  process.env.PROD_MONGO 
-  :process.env.DEV_MONGO
+  process.env.DEV_MONGO
+  :process.env.PROD_MONGO 
   
   try {
     await mongoose.connect(uri);
